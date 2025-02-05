9c9397fcafbdd8282b401aa078308da0ac7d97c2
Maks00071 <corystov.maks@yandex.ru>
2025-02-05 22:00:33 +0300

    CHANGE RDDNoteb 05/02/2025

--- src/main/scala/RDDNotebook/RDDNoteb.scala
+++ src/main/scala/RDDNotebook/RDDNoteb.scala
@@ -13,12 +13,12 @@ object RDDNoteb {
 
     println("------------- spark.version -------------")
 
-    println(s"spark.version = ${sc.version}")  // spark.version = 3.5.0
+    println(s"spark.version = ${sc.version}") // spark.version = 3.5.0
 
     println("---------- map --------------")
 
     // map
-    val rddMap = sc.parallelize(List("a","b","c","d","e","f","g","x","y","z"), 3)
+    val rddMap = sc.parallelize(List("a", "b", "c", "d", "e", "f", "g", "x", "y", "z"), 3)
     val mappedRDD = rddMap.map(_.toUpperCase)
     println(mappedRDD.collect().mkString(",")) // "A,B,C,D,E,F,G,X,Y,Z"
 
@@ -47,7 +47,7 @@ object RDDNoteb {
 
     // mapPartitionsWithIndex
     val rddMappartitionsWithIndex = sc.parallelize(List("a", "b", "c", "d", "e", "f", "x", "y", "z"), 3)
-    val mapPartitionsWithRDD = rddMappartitionsWithIndex.mapPartitionsWithIndex((i,j) => Array(i).iterator)
+    val mapPartitionsWithRDD = rddMappartitionsWithIndex.mapPartitionsWithIndex((i, j) => Array(i).iterator)
     println(mapPartitionsWithRDD.collect().mkString("/")) // "0/1/2"
 
     println("----------- sample -------------")
@@ -87,7 +87,7 @@ object RDDNoteb {
     println("------------ groupByKey ------------")
 
     // groupByKey
-    val rddGroupByKey = sc.parallelize(List((1,"a"),(2,"b"),(3,"c"),(1,"d"),(2,"e")), 3)
+    val rddGroupByKey = sc.parallelize(List((1, "a"), (2, "b"), (3, "c"), (1, "d"), (2, "e")), 3)
     val groupByKeyRDD = rddGroupByKey.groupByKey()
     println(groupByKeyRDD.collect().mkString("/")) // (3,CompactBuffer(c))/(1,CompactBuffer(a, d))/(2,CompactBuffer(b, e))
 
@@ -222,6 +222,7 @@ object RDDNoteb {
 
     println("------------------------")
 
+  }
 }
 
 
